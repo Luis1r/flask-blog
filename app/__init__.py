1b2d7a430335a58c94a66eead8bf528e0eaf5ce5
Luis Silva <luis.rob.sil@gmail.com>
2020-08-26 23:56:39 -0500

    version 3

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+import urllib
+
+login_manager = LoginManager()
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+    params = urllib.parse.quote_plus('DRIVER={SQL Server};SERVER=DESKTOP-DUHA34C;DATABASE=miniblog;Trusted_Connection=yes;')
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+    db.init_app(app)
+    # Registro de los Blueprints
+    from .auth import auth_bp
+    app.register_blueprint(auth_bp)
+    from .admin import admin_bp
+    app.register_blueprint(admin_bp)
+    from .public import public_bp
+    app.register_blueprint(public_bp)
+    return app
