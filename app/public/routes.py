51478954d51200fd26d803ff8994a0326501bd46
Luis Silva <luis.rob.sil@gmail.com>
2020-08-31 23:56:14 -0500

    version3.5

--- app/public/routes.py
+++ app/public/routes.py
@@ -1,10 +1,15 @@
 from flask import abort, render_template
+
 from app.models import Post
 from . import public_bp
+
+
 @public_bp.route("/")
 def index():
     posts = Post.get_all()
     return render_template("public/index.html", posts=posts)
+
+
 @public_bp.route("/p/<string:slug>/")
 def show_post(slug):
     post = Post.get_by_slug(slug)
